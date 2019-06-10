6966baedd3f7589b7e409b097c850645d6cc9bad
9vit917 <9vit917@gmail.com>
2019-06-10 18:03:14 +0300

    addTest

--- smilewidgets/products/tests.py
+++ smilewidgets/products/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from .models import ProductPrice, Product, GiftCard
+
+c = Client()
+
+class MyTest(TestCase):
+    def makeSettings(self):
+        product_big = Product.objects.create(
+            name="Big Widget",
+            code="big_widget"
+        )
+        GiftCard.objects.create(
+            code="10OFF",
+            amount=1000,
+            date_start="2018-07-01",
+            date_end=None
+        )
+        ProductPrice.objects.create(
+            name="default",
+            price=100000,
+            product=product_big
+        )
+
+    def testForBigProduxt(self):
+        response = c.get('/api/get-price', {
+            'productCode': 'big_widget', 
+            'date': '2018-07-01', 
+            'giftCardCode': '10OFF'
+        })
+        self.assertEqual(response.status_code, 200)
